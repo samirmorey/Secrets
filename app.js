d53563a87a974d5ed36ef43f37206875c2abb70e
samirmorey <73993138+samirmorey@users.noreply.github.com>
2020-12-18 13:01:55 +0530

    Adding configs and scripts for running

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(session({           //necessary to put above DB connection
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true,useUnifiedTopology:true});
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true,useUnifiedTopology:true});
 mongoose.set("useCreateIndex",true);
 const userSchema= new mongoose.Schema({
   email:String,
